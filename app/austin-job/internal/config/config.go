82e9a70f8fefa4f09d4181414f60b4361978f15e
wanders <13834563@qq.com>
2022-09-30 13:45:20 +0800

    s

--- app/austin-job/internal/config/config.go
+++ app/austin-job/internal/config/config.go
@@ -3,13 +3,14 @@ package config
 import (
 	"austin-go/common/dbx"
 	"austin-go/common/mq"
+
 	"github.com/zeromicro/go-zero/core/stores/cache"
 	"github.com/zeromicro/go-zero/rest"
 )
 
 type Config struct {
 	rest.RestConf
-	Rabbit     mq.RabbitConf
-	CacheRedis cache.CacheConf // redis缓存
 	Mysql      dbx.DbConfig
+	Rabbit     mq.RabbitConf
+	CacheRedis cache.CacheConf
 }
